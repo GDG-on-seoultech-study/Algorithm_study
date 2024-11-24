1fd1ad56dae6e8cf04501379fa4944486a3aff10
Chaeyeoncho <gcy9293@naver.com>
2024-11-24 23:54:44 +0900

    11주차 복습과제 조채연

---
+++ 11주차_색인과이진검색트리/복습/11주차_복습과제_조채연.py
@@ -0,0 +1,27 @@
+import sys
+
+def main():
+    n = int(input())
+    nums = sorted(list(map(int, sys.stdin.readline().strip().split())))
+    m = int(input())
+
+    low, high = 0, max(nums) 
+
+    answer = 0 
+    while low <= high:
+        total = 0
+        mid = (low + high) // 2 
+
+        for num in nums: 
+            total += min(num, mid)
+        
+        if total <= m: 
+            low = mid + 1
+            answer = mid
+        else:
+            high = mid - 1
+
+    print(answer)
+
+if __name__ == '__main__':
+    main()
